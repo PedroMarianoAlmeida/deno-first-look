00d4c50ac899790a39cb5d9f72949a5ec81fa5d6
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-09 13:45:37 -0500

    importing react

---
+++ my_project/main.tsx
@@ -0,0 +1,9 @@
+import React from "https://esm.sh/react";
+import { renderToString } from "https://esm.sh/react-dom/server";
+
+const App = () => <h1>Hello from React</h1>;
+
+Deno.serve((_req) => {
+  const html = renderToString(<App />);
+  return new Response(html);
+});
