d87d3ee7ef2cbc1b60e66ba7e55a984ea9bcb6bf
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-09 14:33:55 -0500

    route

--- main.tsx
+++ main.tsx
@@ -1,10 +1,24 @@
 import React from "https://esm.sh/react";
 import { renderToString } from "https://esm.sh/react-dom/server";
 
-const App = () => <h1>Hello from React</h1>;
+const AppRoot = () => (
+  <h1 style={{ backgroundColor: "red" }}>Hello from React</h1>
+);
+const About = () => <h1>About</h1>;
 
 Deno.serve((_req) => {
-  const html = renderToString(<App />);
+  const url = new URL(_req.url);
+
+  const routesToComponents: Record<string, JSX.Element> = {
+    "/": <AppRoot />,
+    "/about": <About />,
+    "not-found": <h1>Not Found</h1>,
+  };
+
+  const component =
+    routesToComponents[url.pathname] || routesToComponents["not-found"];
+
+  const html = renderToString(component);
   return new Response(html, {
     headers: { "Content-Type": "text/html" },
   });
