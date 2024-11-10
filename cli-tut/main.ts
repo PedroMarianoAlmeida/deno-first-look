b9d5cf5bad00cea612360cdff6153fb8ec34619a
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-10 14:47:54 -0500

    reading user data
    To put path input

--- cli-tut/main.ts
+++ cli-tut/main.ts
@@ -4,10 +4,33 @@ const args = parseArgs(Deno.args, {
   alias: {
     path: "p",
   },
-  default: {
-    path: "./",
-  },
 });
 
-const entryPoint = args.path;
-console.log("The entrypoint is: ", entryPoint);
+const { path } = args;
+console.log(path);
+
+if (!path) {
+  const buf = new Uint8Array(1024);
+  console.log("Add path");
+
+  const n = await Deno.stdin.read(buf) || undefined;
+  const input = new TextDecoder().decode(buf.subarray(0, n)).trim();
+
+  console.log(`You entered: ${input}`);
+}
+
+
+// using file = await Deno.open(path, { read: true });
+// const fileInfo = await file.stat();
+
+// console.log("-------------BEGIN---------------")
+// console.log({fileInfo});
+// if (fileInfo.isFile) {
+//   const buf = new Uint8Array(100);
+//   const text = new TextDecoder().decode(buf);
+//   console.log("File text: ", text);
+// }
+
+// const entryPoint = args.path;
+// console.log("The entrypoint is: ", entryPoint);
+// console.log("------------END----------------")
