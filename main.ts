f08c726bae30f4ea0997c5d931440ed34a2c0032
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-12 16:28:35 -0500

    giving um of monorepo

--- main.ts
+++
@@ -1,6 +0,0 @@
-import chalk from "chalk";
-import { add } from "@scope/add";
-import { subtract } from "@scope/subtract";
-
-console.log("1 + 2 =", chalk.green(add(1, 2)));
-console.log("2 - 4 =", chalk.red(subtract(2, 4)));
