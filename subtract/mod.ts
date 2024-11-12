50c12658bf1379e651ff2b45b93f342aed377d33
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-11-12 16:45:01 -0500

    using add jsr published on subtract project

--- subtract/mod.ts
+++ subtract/mod.ts
@@ -1,5 +1,7 @@
-import { add } from "@scope/add";
+import { add } from "jsr:@palmeida/add";
 
 export function subtract(a: number, b: number): number {
   return add(a, b * -1);
 }
+
+console.log(subtract(5, 2));
